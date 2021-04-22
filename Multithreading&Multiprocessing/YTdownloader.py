ffb87147554f6f63b05f4703f2e71cdc3e0180f3
Sai Prasanth V <sai230520@gmail.com>
2021-04-22 17:00:33 +0530

    added yt_download feature

---
+++ Multithreading&Multiprocessing/YTdownloader.py
@@ -0,0 +1,15 @@
+
+from pytube import YouTube
+
+youtube_video_url = 'https://www.youtube.com/watch?v=668nUCeBHyY'
+
+try:
+    yt_obj = YouTube(youtube_video_url)
+
+    filters = yt_obj.streams.filter(progressive=True, file_extension='mp4')
+
+    # download the highest quality video
+    filters.get_highest_resolution().download()
+    print('Video Downloaded Successfully')
+except Exception as e:
+    print(e)
