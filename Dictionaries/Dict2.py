744a65a4913c7248e339b64b6b6f621bd11cb1db
Sai Prasanth V <sai230520@gmail.com>
2021-04-27 18:03:21 +0530

    following PEP8 format

--- Dictionaries/Dict2.py
+++ Dictionaries/Dict2.py
@@ -6,6 +6,7 @@ def character_frequency(string):
     dictionary = dict()
     print('Frequency of Characters:')
     for letter in string:
+        # Counting the count of characters in the string
         dictionary[letter] = dictionary.get(letter, 0)+1
     print(dictionary)
 
