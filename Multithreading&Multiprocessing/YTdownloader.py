3faa5716fffcfd132ff7bb9d1ac7b5d52d5f4182
Sai Prasanth V <sai230520@gmail.com>
2021-04-27 18:13:48 +0530

    added comments

--- Multithreading&Multiprocessing/YTdownloader.py
+++ Multithreading&Multiprocessing/YTdownloader.py
@@ -1,20 +1,3 @@
-
-# from pytube import YouTube
-
-# youtube_video_url = 'https://www.youtube.com/watch?v=668nUCeBHyY'
-
-# try:
-#     yt_obj = YouTube(youtube_video_url)
-
-#     filters = yt_obj.streams.filter(progressive=True, file_extension='mp4')
-
-#     # download the highest quality video
-#     filters.get_highest_resolution().download()
-#     print('Video Downloaded Successfully')
-# except Exception as e:
-#     print(e)
-
-
 from pytube import YouTube
 import logging
 import threading
@@ -24,19 +7,23 @@ file_size = 0
 
 
 def complete_func(stream, file_handle):
+    # function to indicate that the download is completed
     print('Video downloaded successfully !')
 
 
 def show_progress_bar(chunk, file_handle, bytes_remaining):
+    # function to show the progress of downloads
     global file_size
     if file_size == 0:
         file_size = bytes_remaining
     else:
         r = float(file_size - bytes_remaining)
+        # calculates the download percentage
         print(str(round(r/float(file_size)*100, 0)) + '% of video downloaded')
 
 
 def thread_function(url):
+    # thread function to perform Multithreading
     logging.info("Thread %s: starting", url)
     yt = YouTube(url)
     print('Going to download:', yt.title)
@@ -50,17 +37,14 @@ if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
-
+    # list of YuoTube URL links
     YT_url_list = ['https://www.youtube.com/watch?v=qiwBjYhQ_-c&list=PL-FbhHsDM2qH4JS70UuUxa4lVJZn4bsLD&index=4',
                    'https://www.youtube.com/watch?v=movsj_dyrOQ&list=PL-FbhHsDM2qH4JS70UuUxa4lVJZn4bsLD&index=6']
 
     for url in YT_url_list:
-        # print('url=  ', url)
-
         logging.info("Main    : before creating thread")
         x = threading.Thread(target=thread_function, args=(url,))
         logging.info("Main    : before running thread")
         x.start()
         logging.info("Main    : wait for the thread to finish")
-        # x.join()
         logging.info("Main    : all done")
