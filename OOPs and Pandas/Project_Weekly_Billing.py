f167db0efa1aaec71abf4fe121004fe6894705d4
Sai Prasanth V <sai230520@gmail.com>
2021-04-28 08:44:40 +0530

    added comments

--- OOPs and Pandas/Project_Weekly_Billing.py
+++ OOPs and Pandas/Project_Weekly_Billing.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 
 class Project():
     def __init__(self, df):
+        '''
+        arg -> dataframe (df)
+        task -> calculates multiple summary reports
+        '''
         self.df = df
         self.employee = df.User.unique()
         self.tags = df.Tag.unique()
@@ -23,17 +27,27 @@ class Project():
         return(barG.plot(kind='bar', cmap='Accent'))
 
 
+# reading csv file
 df = pd.read_csv(r"OOPs and Pandas\timesheet.csv")
 # df = pd.read_csv("timesheet.csv")
 
+# creating a constant for USR to INR conversion
+INR_USR_constant = 74.53
+
+# removing the rows and columns in which all values are filled with 'None'
 df = df.dropna(how='all', axis='rows')
 df = df.dropna(how='all', axis='columns')
+
+# removing the last two lines which has the total summary of the sheet
 df = df[:223]
 
+# creating column for 'Billing Amount in USD' and entering value as 25 for Employee and 40 for other
 df['Billing Amount in USD'] = np.where(df['Role'].str == 'Employee', 25, 40)
 
-df['Billing Amount in INR'] = df['Billing Amount in USD']*74.53
+# creating column for 'Billing Amount in INR' and entering respective Billing value in INR
+df['Billing Amount in INR'] = df['Billing Amount in USD']*INR_USR_constant
 
+# converting the valuue of 'Hours(For Calculation)' from String to Float
 df['Hours(For Calculation)'] = pd.to_numeric(df['Hours(For Calculation)'])
 
 print(df)
