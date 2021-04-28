dba97793c46bddcb6956058391b9a2a0e3e04fd9
Sai Prasanth V <sai230520@gmail.com>
2021-04-28 09:54:29 +0530

    commenting & simplifying the code

--- Flask tasks/app.py
+++ Flask tasks/app.py
@@ -9,9 +9,14 @@ PATH = 'uploads'
 
 app = Flask(__name__)
 app.secret_key = "secret key"
+
+# configuring the upload file size limit -> 10 MB
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
+
+# allowed file formats
 app.config['UPLOAD_EXTENSIONS'] = [
     '.jpg', '.png', '.gif', '.txt', '.pdf', '.jpeg']
+# configuring the default uploa path
 app.config['UPLOAD_PATH'] = PATH
 
 
@@ -29,59 +34,51 @@ def upload_files():
         uploaded_file = request.files['file']
         filename = secure_filename(uploaded_file.filename)
         if filename != '':
+            # checking the file format by extecting file extension from file name
             file_ext = os.path.splitext(filename)[1].lower()
             if file_ext not in app.config['UPLOAD_EXTENSIONS']:
                 flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
                 # abort(400)
             else:
+                # saving the file configured Upload path
                 uploaded_file.save(os.path.join(
                     app.config['UPLOAD_PATH'], filename))
                 flash('File successfully uploaded', category='success')
         return redirect(url_for('index'))
 
     elif request.form.get('Size'):
+        # displaying the size of uploaded file
         return render_template('index.html', files=files, size=view_size())
 
     elif request.form.get('Date_Modified'):
+        # displaying the modified date of uploaded file
         return render_template('index.html', files=files, mdate=view_mdate())
 
     elif request.form.get('No_of_line'):
+        # displaying the number of line in uploaded file
         return render_template('index.html', files=files, n_lines=view_nlines())
 
     return redirect(url_for('index'))
 
 
-# @app.route('/data', methods=['GET', 'POST'])
-# def datas():
-#     my_dir = PATH
-
-#     for f in os.listdir(my_dir):
-#         print(f)
-#         flash(f, category='success')
-#         path = os.path.join(my_dir, f)
-#         if os.path.isfile(path):
-#             print("last modified: %s" % time.ctime(os.path.getmtime(path)))
-#             print(os.path.getsize(path), 'bytes')
-
-#     # return redirect(url_for('index'), data='dummy')
-#     return render_template('index.html', data='dummy')
-
-
 @app.route('/uploads/<filename>')
 def upload(filename):
+    # displaying the uploaded files
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
 
 
 def view_size():
+    # returns the size of particular file
     path = os.path.join(PATH, uploaded_file.filename)
     if os.path.isfile(path):
         size = str(os.path.getsize(path))
-        return(size + 'Bytes')
+        return(size + ' Bytes')
     else:
         return None
 
 
 def view_mdate():
+    # returns the modified date of particular file
     path = os.path.join(PATH, uploaded_file.filename)
     if os.path.isfile(path):
         mdate = str(time.ctime(os.path.getmtime(path)))
@@ -91,20 +88,11 @@ def view_mdate():
 
 
 def view_nlines():
+    # return the number of lines in a file
     path = os.path.join(PATH, uploaded_file.filename)
     if os.path.isfile(path):
-        # nlines = len(open(uploaded_file.filename).readlines())
-        count = 0
-        with (open(path, "r") as reader):
-            content = reader.read()
-            colist = content.split("\n")
-
-        for lines in colist:
-            if lines:
-                count += 1
-        count = str(count)
-        return ("Number of lines: " + count)
-        # return('No of lines: ' + str(nlines))
+        nlines = len(open(path).readlines())
+        return('Number of lines: ' + str(nlines))
     else:
         return None
 
